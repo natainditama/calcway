c40b153d62eca26dcad425c479ac9b7d0987ac32
natainditama <natainditama@gmail.com>
2022-03-28 05:48:25 +0800

    Release docs/init: Create calcway new project

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import '/assets/styles/main.css'
+
+const app = document.querySelector<HTMLDivElement>('#app')!
+
+app.innerHTML = `
+  <h1>Hello Vite!</h1>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+`
