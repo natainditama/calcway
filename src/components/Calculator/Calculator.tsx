7a2c7d0a065cf7c505136a5924e06a27e91681f5
natainditama <natainditama@gmail.com>
2023-01-07 10:33:52 +0800

    feat: Updated calculator

--- src/components/Calculator/Calculator.tsx
+++ src/components/Calculator/Calculator.tsx
@@ -1,4 +1,4 @@
-import React, { createRef, useEffect, useState, memo, useCallback } from "react";
+import React, { createRef, useEffect, useState, memo } from "react";
 import Screen from "../Screen/Screen";
 import Keypad from "../Keypad/Keypad";
 
@@ -9,16 +9,15 @@ function Calculator() {
   const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
   const ref = createRef<HTMLParagraphElement>();
 
-  const updateCalc = useCallback((value: string) => {
+  const updateCalc = (value: string) => {
     const displayValue = ref.current as HTMLParagraphElement;
-    if (value === "0" && calc === "0") {
-      return clear();
-    } else if (
+    if (value === "0" && calc === "0") return clear();
+    else if (
       (operations.includes(value) && calc === "") ||
       (operations.includes(value) && operations.includes(calc.slice(-1)))
-    ) {
+    )
       return;
-    } else if (!operations.includes(value)) {
+    else if (!operations.includes(value)) {
       setCalc((+calc + +value).toString());
 
       if (calc.length > 7) {
@@ -47,39 +46,36 @@ function Calculator() {
       console.error();
     }
     setCalc(calc + value);
-  }, []);
+  };
 
-  const calculate = useCallback(() => {
+  const calculate = () => {
     if (eval(calc) === undefined) return;
     setCalculated(true);
     setCalc(eval(calc).toString());
-  }, []);
+  };
 
-  const clear = useCallback(() => {
+  const clear = () => {
     setCalculated(false);
     setCalc("");
-  }, []);
+  };
 
-  const deleteCalc = useCallback(() => {
-    if (calc === "") {
-      return;
-    }
+  const deleteCalc = () => {
+    if (calc === "") return;
     const displayValue = ref.current as HTMLParagraphElement;
     if (calc.length < 7) {
       displayValue.style.fontSize = "3.3rem";
     }
     const value = calc.slice(0, -1);
     setCalc(value);
-  }, []);
+  };
 
-  const onKeyDown = useCallback((event: KeyboardEvent): void => {
-    console.log(event);
+  const onKeyDown = (event: KeyboardEvent): void => {
     if (operations.includes(event.key) || numbers.includes(event.key)) {
       updateCalc(event.key);
     } else if (event.key === "Enter" || event.key === "=") {
       calculate();
     } else if (event.key === "Backspace") deleteCalc();
-  }, []);
+  };
 
   useEffect(() => {
     window.addEventListener("keydown", onKeyDown);
@@ -104,4 +100,4 @@ function Calculator() {
   );
 }
 
-export default memo(Calculator)
+export default memo(Calculator);
