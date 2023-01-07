7371f042f001d37d10abe3c2c5e32b5c674fc345
natainditama <natainditama@gmail.com>
2023-01-07 13:05:38 +0800

    feat: Destructured components

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,5 +3,6 @@ import Header from "./Header/Header";
 import Keypad from "./Keypad/Keypad";
 import Screen from "./Screen/Screen";
 import ThemeButtons from "./ThemeButtons/ThemeButtons";
+import DefaultSeo from "./Seo/DefaultSeo";
 
-export { Calculator, Header, Keypad, Screen, ThemeButtons };
+export { Calculator, Header, Keypad, Screen, ThemeButtons, DefaultSeo };
