72ce33d42891bc38b69e6d81a3824531f98c54fd
natainditama <natainditama@gmail.com>
2023-01-07 10:35:37 +0800

    fix: Destructured components

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+import Calculator from "./Calculator/Calculator";
+import Header from "./Header/Header";
+import Keypad from "./Keypad/Keypad";
+import Screen from "./Screen/Screen";
+import ThemeButtons from "./ThemeButtons/ThemeButtons";
+
+export { Calculator, Header, Keypad, Screen, ThemeButtons };
