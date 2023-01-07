a5f6b35bf163492f811c934cd77a663899d37e2a
natainditama <natainditama@gmail.com>
2023-01-07 09:38:29 +0800

    feat: Setup vite-plugin-webfont

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
+import webfontDownload from 'vite-plugin-webfont-dl';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(), 
+    webfontDownload([
+    'https://fonts.googleapis.com/css2?family=League+Spartan:wght@700&display=swap',
+    ])
+  ],
   resolve: {
     alias: [
       {
