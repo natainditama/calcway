2c1f2d1ce97689dff67b470b728e66c8ecfcaf6a
natainditama <natainditama@gmail.com>
2023-01-07 09:12:51 +0800

    feat: Implement header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ThemeButtons from "../ThemeButtons/ThemeButtons";
+import { ThemeProps } from "@/styles/theme";
+
+type HeaderProps = {
+  handleThemeChange: (theme: ThemeProps) => void;
+};
+
+function Header({ handleThemeChange }: HeaderProps) {
+  return (
+    <header>
+      <h1>calcway</h1>
+      <section className="themes">
+        <p>THEME</p>
+        <ThemeButtons handleThemeChange={handleThemeChange} />
+      </section>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/ThemeButtons/ThemeButtons.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { ThemeProps, basic, light, dark } from "@/styles/theme";
+import styled from "styled-components";
+
+export type ThemeButtonsProps = {
+  handleThemeChange: (theme: ThemeProps) => void;
+};
+
+export const ThemeButton = styled.input`
+  background-color: transparent;
+  border: none;
+  border-radius: 50%;
+  width: 1rem;
+  height: 0.8rem;
+  cursor: pointer;
+`;
+
+export default function ThemeButtons({ handleThemeChange }: ThemeButtonsProps) {
+  const themes = [basic, light, dark];
+  return (
+    <nav className="theme-toggle">
+      <section className="theme-number">
+        {themes.map(function (theme: ThemeProps, index: number) {
+          return (
+            <label htmlFor={theme.name} className="theme-number" key={theme.name}>
+              {index + 1}
+            </label>
+          );
+        })}
+      </section>
+      <section className="theme-value">
+        {themes.map(function (theme: ThemeProps, index: number) {
+          return (
+            <ThemeButton
+              aria-label={theme.name}
+              type={"button"}
+              id={theme.name}
+              key={theme.name}
+              className={`theme-btn ${theme.name} ${index == 0 ? "active" : null}`}
+              onClick={() => handleThemeChange(theme)}
+            ></ThemeButton>
+          );
+        })}
+      </section>
+    </nav>
+  );
+}
